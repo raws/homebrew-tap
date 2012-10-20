13c7115136436afa11d60f3e9680b6b5bc598929
Ross Paffett <ross@rosspaffett.com>
2012-10-20 18:38:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-tap
+
+This repository contains my personal [Homebrew](https://github.com/mxcl/homebrew) formulae.
+
+## Usage
+
+To use any of the formulae in this repository, simply:
+
+```
+brew tap raws/homebrew-tap
+brew install <formula>
+```
+
+You can also install a specific formula via its URL:
+
+```
+brew install https://raw.github.com/raws/homebrew-tap/master/<formula>.rb
+```
---
+++ splunk-forwarder.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class SplunkForwarder < Formula
+  homepage 'http://www.splunk.com'
+  url 'http://www.splunk.com/page/download_track?file=4.3.4/universalforwarder/osx/splunkforwarder-4.3.4-136012-Darwin-universal.tgz&ac=&wget=true&name=wget&typed=releases'
+  sha1 '7a6074edf8f67f442b9a2e853ba2912a1283731a'
+
+  def install
+    prefix.install executables
+    prefix.install support_files
+  end
+
+  def test
+    system 'splunk'
+  end
+
+  private
+
+  def executables
+    Dir['bin/*'] - %w(scripts setSplunkEnv)
+  end
+
+  def support_files
+    Dir['{etc,lib,openssl,share}/*']
+  end
+end
