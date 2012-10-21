8881c88198cf70e172c6f7dfa5ef648858d78c44
Ross Paffett <ross@rosspaffett.com>
2012-10-21 14:21:58 -0400

    splunk-forwarder: Create wrapper executable that sets SPLUNK_HOME

--- splunk-forwarder.rb
+++ splunk-forwarder.rb
@@ -1,4 +1,5 @@
 require 'formula'
+require 'shellwords'
 
 class SplunkForwarder < Formula
   homepage 'http://www.splunk.com'
@@ -6,8 +7,8 @@ class SplunkForwarder < Formula
   sha1 '7a6074edf8f67f442b9a2e853ba2912a1283731a'
 
   def install
-    prefix.install executables
-    prefix.install support_files
+    install_support_files
+    install_executables
   end
 
   def test
@@ -16,11 +17,33 @@ class SplunkForwarder < Formula
 
   private
 
-  def executables
-    Dir['bin/*'] - %w(scripts setSplunkEnv)
+  def auxiliary_executables
+    Dir['bin/*'] - %w(bin/splunk bin/scripts bin/setSplunkEnv)
   end
 
-  def support_files
-    Dir['{etc,lib,openssl,share}/*']
+  def install_executables
+    bin.install auxiliary_executables
+    libexec.install 'bin/splunk'
+    install_executable_wrapper
+  end
+
+  def install_executable_wrapper
+    bin.join('splunk').tap do |wrapper|
+      wrapper.open 'w' do |f|
+        f.puts <<-WRAPPER
+          #!/usr/bin/env SPLUNK_HOME=#{prefix.to_s.shellescape}
+          #{real_executable.to_s.shellescape} "$@"
+        WRAPPER
+      end
+      wrapper.chmod 0755
+    end
+  end
+
+  def install_support_files
+    prefix.install %w(etc lib openssl)
+  end
+
+  def real_executable
+    libexec.join 'splunk'
   end
 end
